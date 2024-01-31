8300248a4b5e8feebbabc604797b8a4e09a0e6de
Guillermo Tapia <guillermotf23@gmail.com>
2024-01-31 15:49:29 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { Box, Container, TextField, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import DetailsWeather from "./components/DetailsWeather";
+
+const BASE_WEATHER_API = `http://api.weatherapi.com/v1/current.json?key=${
+  import.meta.env.VITE_API_KEY
+}&lang=es&q=`;
+
+function App() {
+  const [city, setCity] = useState(0);
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState({
+    message: "",
+    error: false,
+  });
+  const [weather, setWeather] = useState({
+    city: "",
+    country: "",
+    temp: "",
+    condition: "",
+    icon: "",
+    conditionText: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    console.log("funciona");
+
+    console.log(city);
+
+    try {
+      if (!city.trim()) throw { message: "Campo ciudad es obligatorio" };
+
+      const response = await fetch(BASE_WEATHER_API + city);
+      const data = await response.json();
+
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      });
+    } catch (error) {
+      console.log(error);
+      setError({
+        message: error.message,
+        error: true,
+      });
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="WeatherApp">
+        <Container maxWidth="xs" sx={{ mt: 2 }}>
+          <Typography variant="h3" component="h1" align="center" gutterBottom>
+            Weather-Api-App
+          </Typography>
+
+          <Box
+            sx={{ display: "grid", gap: 2 }}
+            component="form"
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              id="city"
+              label="Ciudad"
+              variant="outlined"
+              size="small"
+              required
+              fullWidth
+              onChange={(event) => setCity(event.target.value)}
+              error={error.error}
+              helperText={error.message}
+            />
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              loading={isLoading}
+              loadingIndicator="Searching..."
+            >
+              Buscar
+            </LoadingButton>
+          </Box>
+
+          {weather.city && <DetailsWeather weather={weather} />}
+
+          <Typography textAlign="center" sx={{ mt: 2, fontSize: "10px" }}>
+            Powered by{" "}
+            <a target="_blanc" href="https://www.weatherapi.com/">
+              WeatherApi.com
+            </a>
+          </Typography>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DetailsWeather.jsx
@@ -0,0 +1,34 @@
+import { Typography,Box, } from "@mui/material";
+
+const DetailsWeather = ({weather}) => {
+  return (
+    <div className="weather">
+           <Box
+            sx={{
+              mt: 2,
+              display: "grid",
+              gap: 2,
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="h4" component="h2">
+              {weather.city}, {weather.country}
+            </Typography>
+            <Box
+              component="img"
+              alt={weather.conditionText}
+              src={weather.icon}
+              sx={{ margin: "0 auto" }}
+            ></Box>
+            <Typography variant="h5" component="h3">
+              {weather.temp}°C
+            </Typography>
+            <Typography variant="h6" component="h4">
+              {weather.conditionText}
+            </Typography>
+          </Box>
+    </div>
+  )
+}
+
+export default DetailsWeather
